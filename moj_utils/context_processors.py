38fcdfb36eafcd25e07f9d8605e4fd178e910854
Max Froumentin <max.froumentin@digital.justice.gov.uk>
2015-10-08 11:03:30 +0100

    adds a context processor for analytics [updates #103597472]

--- moj_utils/context_processors.py
+++ moj_utils/context_processors.py
@@ -3,3 +3,6 @@ from django.conf import settings
 
 def debug(request):
     return {'DEBUG': settings.DEBUG}
+
+def analytics(request):
+    return {'GOOGLE_ANALYTICS_ID': getattr(settings, 'GOOGLE_ANALYTICS_ID', None)}
