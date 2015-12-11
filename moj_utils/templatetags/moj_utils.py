55e21fe0f7766a3775c113c5b576a4ec1af851ad
Igor <igor.ushkarev@digital.justice.gov.uk>
2015-12-11 17:40:07 +0000

    Add a template tag for including Sentry JS exception monitoring

--- moj_utils/templatetags/moj_utils.py
+++ moj_utils/templatetags/moj_utils.py
@@ -17,3 +17,16 @@ def safewrap(val, arg):
 def field_from_name(form, name):
     if name in form.fields:
         return form[name]
+
+
+@register.inclusion_tag('moj_utils/sentry-js.html')
+def sentry_js():
+    try:
+        from raven.contrib.django.models import client
+    except ImportError:
+        return None
+
+    sentry_dsn = client.get_public_dsn('https') or None
+    return {
+        'sentry_dsn': sentry_dsn
+    }
